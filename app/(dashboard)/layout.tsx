fbcf9112f4a5739a81eef86d55702bd9843fb73a
christiannshuti <chrisnshuti943@gmail.com>
2026-06-07 18:58:24 +0200

    Modified dashboard layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,25 @@
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
+import { ProjectProvider } from "@/context/ProjectContext";
 
 export default function DashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <div style={{ display: "flex", minHeight: "100vh" }}>
-            <Sidebar />
+  return (
+    <ProjectProvider>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
 
-            <div style={{flex:1}}>
-                <Navbar />
-                <main style={{padding: "20px" , background: "#f8fafc" , minHeight: "100vh" }}>
-                    {children}
-                </main>
-            </div>
+        <div style={{ flex: 1 }}>
+          <Navbar />
+
+          <main style={{ padding: "20px", background: "#f8fafc", minHeight: "100vh" }}>
+            {children}
+          </main>
         </div>
-    );
+      </div>
+    </ProjectProvider>
+  );
 }
